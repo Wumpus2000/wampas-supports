423b50219d3417a9b94b13bb9a9d6823fd9c8403
Wumpus2000 <gaameer923@gmail.com>
2022-04-02 01:43:28 -0600

    change

--- slashCommands/Tickets/send-panel.js
+++ slashCommands/Tickets/send-panel.js
@@ -23,15 +23,15 @@ module.exports = {
             .setStyle("DANGER")
             .setLabel('Support')
             .setEmoji("952041245910396968")
-            .setCustomId("create-ticket")
-        );
+            .setCustomId("create-ticket"));
 
+    
         const embed = new client.discord.MessageEmbed()
         .setTitle("Open a Support Ticket!")
-        .setDescription("<:channel:942191114935492619> If you are experiencing a **bug** and need assistance troubleshooting the issue, our team is dedicated to providing a solution. Simply click the button below.\n\n<:channel:942191114935492619> If you have witnessed someone covertly breaking our **Server Rules**, our team is dedicated to providing a safe environment. Simply click the button below.\n\n<:CircleRed:957390667573764206> Please understand that our staff is comprised of volunteers and that it could, in extreme cases, take up to **24-hours** to respond, sometimes it depends if we're busy or not. ")
+        .setDescription("<:Threads:939808444515504148> If you are experiencing a **Bug(s)** and need assistance troubleshooting the issue, our team is dedicated to providing a solution. Simply click the button below.\n\n<:Threads:939808444515504148> If you have witnessed someone covertly breaking our **Server Rules**, our team is dedicated to providing a safe environment. Simply click the button below.\n\n<:CircleNotice:959713711071838228> Please understand that our staff is comprised of volunteers and that it could, *in extreme cases,* take up to **24-Hours** to respond. *However, the average response-time is within an hour.* Thank you, in advance, for your patience and we appreciate your interest in making Wampas a part of your Discord Server. ")
         .setColor('#ED4245')
         .setThumbnail('https://cdn.discordapp.com/attachments/939720531492605963/958108053700493353/lofi-wumpus_copy.png')
-        .setAuthor({ name: 'Wampas Support', iconURL: 'https://cdn.discordapp.com/attachments/939720531492605963/958108053700493353/lofi-wumpus_copy.png', url: 'https://discord.com/channels/93972055' })
+        .setAuthor({ name: 'Wampas Support', iconURL: 'https://cdn.discordapp.com/attachments/939720531492605963/958108053700493353/lofi-wumpus_copy.png', url: 'https://discord.com/channels/939720556700368907/939720556939460660' })
         .setImage('https://cdn.discordapp.com/attachments/939720531492605963/958940264909635614/wampas-etc.png')
         .setFooter({ text: 'Wampas™ • Ticket Tool', iconURL: 'https://cdn.discordapp.com/attachments/939720531492605963/958108053700493353/lofi-wumpus_copy.png' })
 
